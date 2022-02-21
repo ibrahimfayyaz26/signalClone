15516bbf2557fa4293e18ec827cc4ef219bc7c73
Ibrahim <79158349+ibrahimfayyaz26@users.noreply.github.com>
2022-02-21 19:12:45 +0500

    1.8

--- amplify/backend/function/SignalPostConfirmation/src/custom.js
+++ amplify/backend/function/SignalPostConfirmation/src/custom.js
@@ -1,4 +1,38 @@
-exports.handler = (event, context, callback) => {
+const aws = require("aws-sdk");
+const ddb = new aws.DynamoDB();
+
+const tableName = process.env.USERTABLE;
+
+exports.handler = async (event) => {
   // insert code to be executed by your lambda trigger
-  callback(null, event);
+
+  if (!event?.request?.userAttributes?.sub) {
+    console.log("No sub provided");
+    return;
+  }
+
+  const now = new Date();
+  const timeStap = now.getTime();
+
+  const userItem = {
+    __typename: { S: "User" },
+    _lastChangedAt: { N: timeStap.toString() },
+    _version: { N: "1" },
+    updatedAt: { S: now.toISOString() },
+    createdAt: { S: now.toISOString() },
+    id: { S: event.request.userAttributes.sub },
+    name: { S: event.request.userAttributes.email },
+  };
+
+  const params = {
+    Item: userItem,
+    TableName: tableName,
+  };
+
+  try {
+    await ddb.putItem(params).promise();
+    console.log("work");
+  } catch (error) {
+    console.log(error);
+  }
 };
